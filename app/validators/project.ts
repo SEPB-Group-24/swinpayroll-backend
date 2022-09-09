9d0b55eb002517f0f28337c83762c3e4e17923a6
Paves22 <jackpavey00@gmail.com>
2022-09-09 21:54:43 +1000

    Initial commit

---
+++ app/validators/project.ts
@@ -0,0 +1,74 @@
+import { Project } from '../models';
+import Validator, { Type } from './validator';
+
+class ProjectValidator extends Validator<Project> {
+  constructor() {
+    super();
+
+
+    this.addValidation('code', {
+      maxLength: Validator.MAX_STRING_LENGTH,
+      required: true,
+      type: Type.STRING
+    });
+
+    this.addValidation('name', {
+      maxLength: Validator.MAX_STRING_LENGTH,
+      required: true,
+      type: Type.STRING
+    });
+
+    this.addValidation('acronym', {
+      maxLength: Validator.MAX_STRING_LENGTH,
+      required: true,
+      type: Type.STRING
+    });
+
+    this.addValidation('accumulation_amount', {
+      maxLength: Validator.MAX_STRING_LENGTH,
+      required: true,
+      type: Type.STRING
+    });
+
+    this.addValidation('address', {
+      maxLength: Validator.MAX_STRING_LENGTH,
+      required: true,
+      type: Type.STRING
+    });
+
+    this.addValidation('end_date', {
+      custom: ({ addError, value }) => {
+        const date = new Date(value);
+
+        if (isNaN(date.getTime())) {
+          addError('is invalid');
+          return;
+        }
+      },
+      required: true,
+      type: Type.STRING
+    });
+
+    this.addValidation('project_group', {
+      maxLength: Validator.MAX_STRING_LENGTH,
+      required: true,
+      type: Type.STRING
+    });
+
+    this.addValidation('start_date', {
+      custom: ({ addError, value }) => {
+        const date = new Date(value);
+
+        if (isNaN(date.getTime())) {
+          addError('is invalid');
+          return;
+        }
+      },
+      required: true,
+      type: Type.STRING
+    });
+
+  }
+}
+
+export default new ProjectValidator();
