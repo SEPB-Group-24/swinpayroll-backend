16423cc862f4bc277bf6cebdbe3e2585ed237bf5
Jake <89816064+Jake-AS@users.noreply.github.com>
2022-09-25 15:09:59 +1000

    InitChangeOfSubtractorValidatorAndModel
    Plz review.

--- app/validators/subcontract.ts
+++ app/validators/subcontract.ts
@@ -1,4 +1,5 @@
 import { Subcontract } from '../models';
+import { TableName } from '../enums';
 import Validator, { Type } from './validator';
 
 class SubcontractValidator extends Validator<Subcontract> {
@@ -17,18 +18,64 @@ class SubcontractValidator extends Validator<Subcontract> {
       type: Type.STRING
     });
 
-    this.addValidation('down_payment1', {
+    this.addValidation('start_date', {
+      custom: ({ addError, value }) => {
+        const date = new Date(value);
+
+        if (isNaN(date.getTime())) {
+          addError('is invalid');
+          return;
+        }},
+        required: true,
+        type: Type.STRING
+    });
+
+    this.addValidation('project_id', {
+      custom: async ({ addError, value }, database) => {
+        const record = await database.knex(TableName.PROJECTS)
+          .where('id', value)
+          .first();
+
+        if (!record) {
+          addError('is invalid');
+        }
+      },
       required: true,
+      type: Type.STRING
+    })
+
+    this.addValidation('value', {
+      required: true,
+      type: Type.NUMBER
+    })
+
+    this.addValidation('down_payment1', {
+      custom: ({ addError, value }) => {
+        if (value < 0) {
+          addError('must be higher than 0');
+        }
+      },
+      required: false,
       type: Type.NUMBER
     });
 
     this.addValidation('down_payment2', {
-      required: true,
+      custom: ({ addError, value }) => {
+        if (value < 0) {
+          addError('must be higher than 0');
+        }
+      },
+      required: false,
       type: Type.NUMBER
     });
 
     this.addValidation('down_payment3', {
-      required: true,
+      custom: ({ addError, value }) => {
+        if (value < 0) {
+          addError('must be higher than 0');
+        }
+      },
+      required: false,
       type: Type.NUMBER
     });
   }
