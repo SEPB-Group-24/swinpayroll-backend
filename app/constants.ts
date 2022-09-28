b0d1d388ea534fca46ca567f378c5f55ce1c3f96
Dean Levinson <dean.e.levinson@gmail.com>
2022-09-29 00:27:55 +1000

    Payroll history exporting

--- app/constants.ts
+++ app/constants.ts
@@ -1,4 +1,5 @@
 import { path } from 'app-root-path';
 
 export const LOG_FILE_DIR = `${path}/logs`;
+export const TEMPLATES_DIR = `${path}/templates`;
 export const UPLOADS_DIR = `${path}/uploads`;
